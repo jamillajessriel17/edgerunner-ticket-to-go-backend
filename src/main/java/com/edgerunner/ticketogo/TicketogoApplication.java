86c5dfceeca164ef6f87ed4ca6a5b2798307a990
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-09-02 12:04:08 +0800

    init project

---
+++ src/main/java/com/edgerunner/ticketogo/TicketogoApplication.java
@@ -0,0 +1,13 @@
+package com.edgerunner.ticketogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edgerunner/ticketogo/TicketogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgerunner.ticketogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
